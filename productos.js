9e04f2548bf0d1a3e2d22fb851bceb6ed9dc5c7c
mpcitmcastro <116090940+mpcitmcastro@users.noreply.github.com>
2023-03-19 16:34:03 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,63 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Lamparas",
+    variedad: "15w",
+    Marca: "H-Intercom",
+    precio: 1000,
+    img: "/img/Hyundai_LED_15w700x580.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 2,
+    nombre: "Galponeras ALTA POTENCIA",
+    variedad: "30w",
+    Marca: "H-Intercom",
+    precio: 2000,
+    img: "/img/Hyundai_LED_ALTAPOTENCIA30_700x580.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 3,
+    nombre: "Galponeras ALTA POTENCIA",
+    variedad: "S2 30W",
+    Marca: "LH-Intercom",
+    precio: 3000,
+    img: "/img/Hyundai_LED_ALTAPOTENCIA30_S2_700x580.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 4,
+    nombre: "Aplique Interno MPCIT",
+    variedad: "Bidireccional",
+    Marca: "SPOTLINE",
+    precio: 3000,
+    img: "/img/SPL_APLIQUEEXT_2L_GU10_700x580.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 5,
+    nombre: "Embutido Blanco AR111",
+    variedad: "Embutido",
+    Marca: "SPOTLINE",
+    precio: 5000,
+    img: "/img/SPL_BOX4_4L_700x580.jpg",
+    cantidad: 1,
+  },
+
+  {
+    id: 6,
+    nombre: "Lampara AR111",
+    variedad: "AR111",
+    Marca: "BAW",
+    precio: 2000,
+    img: "/img/BAW_AR111_700x450_WEB.jpg",
+    cantidad: 1,
+  },
+];
+
+export default productos;
